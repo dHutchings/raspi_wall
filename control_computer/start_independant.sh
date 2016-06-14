0c6e75b08abad3e33b0e442911e462e2c28f3fdc
Douglas Hutchings <11douglash@berkeley.edu>
2016-06-14 13:28:13 -0700

    Cleanup of start_independant

--- control_computer/start_independant.sh
+++ control_computer/start_independant.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-
-
 #usage
-
 #./start_independant -l LOCATION
 
-
 #Defaults
-VIDEOPATH="."
-echo $VIDEOPATH
-
 LOCATION="" #currently, no location
 
 while [[ $# -gt 1 ]]
@@ -31,8 +24,6 @@ done
 
 echo "starting Monitor #"$LOCATION "Independantly"
 IP=pi@192.168.0.$LOCATION
-
-echo "IP IS " $IP
 UDP=udp://239.0.1.23:1234$LOCATION
 
 sshpass -p "raspberry" ssh $IP "sh -c 'nohup pwomxplayer --tile-code=11 "$UDP"?buffer_size=1200000B > /dev/null 2>&1 &'"
