dd0a6076fcfb8f750151f471fd12cc4da15bcc4e
Douglas Hutchings <11douglash@berkeley.edu>
2016-06-15 12:01:31 -0700

    upgraded run_all_loop
    More gentle killing
    Ability to run forever or only for a set # of loops

--- control_computer/run_all_loop.sh
+++ control_computer/run_all_loop.sh
@@ -1,14 +1,53 @@
 #!/bin/bash
 
-VIDEOPATH="/home/doug/Desktop/disp_case/videos/all_monitors"
+#usage ./run_all_loop -p PATH -l #of loops -f for a "Forever"
+VIDEOPATH=""
+LOOP_COUNT=0
+FOREVER=false
+FFMPEG_PROCESS=""
+while [[ $# -gt 1 ]]
+do
+	key="$1"
+
+	case $key in
+		-p | --path)
+		VIDEOPATH="$2"
+		shift
+		;;
+		-l | --loop)
+		LOOP_COUNT="$2"
+		shift
+		;;
+		-f | --forever)
+		FOREVER=true
+		shift
+		;;
+		*)
+			#unknown option
+		;;
+	esac
+	shift
+done
 
 
+function clean_up {
+	#function to be called at the end, or on interrupt.
+	#i need to Kill the ffmpeg process, since I can't wait for it to end...
+	kill -TERM $FFMPEG_PROCESS
+	/home/doug/Desktop/disp_case/control_computer/stop_all.sh > /dev/null
+	echo "Shutdown all monitors"
+	exit
+}
+
+#magic line here that'll make it easier to ctrl-c out of this thing.
+trap "clean_up" TERM
 
 echo "start"
 prev_width=-1
 prev_height=-1
-#while true; do
-
+while $FOREVER || [[ $LOOP_COUNT -gt "0" ]]; do
+	#echo $FOREVER
+	#echo $LOOP_COUNT
 	for entry in $VIDEOPATH/*
 	do
 		echo $entry
@@ -19,11 +58,12 @@ prev_height=-1
 		width=$(ffprobe -v quiet -show_format -show_streams $entry | grep width | head -1 | cut -d "=" -f2)
 		
 		if [ "$prev_width" -eq "$width" ] && [ "$prev_height" -eq "$height" ]; then
-			echo "resolution ok"
+			:
+			#echo "resolution ok"
 			#do nothing.
 		else
 
-			echo "need to change resolution"
+			#echo "need to change resolution"
 			echo "new resolution is Height" $height "width" $width
 			prev_width=$width
 			prev_height=$height
@@ -34,15 +74,15 @@ prev_height=-1
 
 		#play video
 		#-loglevel panic will cause the thing to display only serious errors.
-		ffmpeg -loglevel panic -re -i $entry -vcodec copy -f avi -an udp://239.0.1.23:1234
+		#dO this complicated buisness so ./run_all_loop.sh exits very cleanly using the 'cleanup function'
+		ffmpeg -loglevel panic -re -i $entry -vcodec copy -f avi -an udp://239.0.1.23:1234 &
+		FFMPEG_PROCESS=$!
+		wait $FFMPEG_PROCESS
+		echo "show video"
 	done
+	LOOP_COUNT=$((LOOP_COUNT - 1))
+
+done
 
 #shutdown all on close
-/home/doug/Desktop/disp_case/control_computer/stop_all.sh > /dev/null
-echo "done"
-
-
-
-
-#done
-#sudo apt-get install libav-tools  for avconv
+clean_up
